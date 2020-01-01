79314b6c44798510dfe24367d7a79fd5f79e57af
LuisDio <luis.diolindo@gmail.com>
2020-01-02 00:02:07 +0300

    initial commit

---
+++ src/test/java/com/luis/lil/springinit/SpringInitApplicationTests.java
@@ -0,0 +1,16 @@
+package com.luis.lil.springinit;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringInitApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
